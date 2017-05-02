fe7430ec1d83bfef6e16af148d72fcfa7f976b1e
ganapati <ganapati@ganapati.com>
2017-05-02 10:53:21 +0200

    Forget to remove debug print

--- RsaCtfTool.py
+++ RsaCtfTool.py
@@ -212,7 +212,6 @@ class RSAAttack(object):
     def pastctfprimes(self):
         path = os.path.dirname(os.path.abspath(__file__))
         pastctfprimes_path = os.path.join(path, 'pastctfprimes.txt')
-        print pastctfprimes_path
         primes = [long(x) for x in open(pastctfprimes_path,'r').readlines() if not x.startswith('#') and not x.startswith('\n')]
         if self.args.verbose:
             print "[*] Loaded " + str(len(primes)) + " primes"
