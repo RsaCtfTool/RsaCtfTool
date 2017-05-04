5e533602de703cd3d8fc7fc31d1d36725808025a
ctfkris <kris@sneakerz.org>
2017-05-04 16:34:34 +1000

    Backport Ganapati's fix for finding the correct path to pastctfprimes.txt to this branch

--- RsaCtfTool.py
+++ RsaCtfTool.py
@@ -21,6 +21,7 @@ from libnum import *
 import requests
 import re
 import argparse
+import os
 import subprocess
 from glob import glob
 
@@ -311,6 +312,8 @@ class RSAAttack(object):
         return
 
     def pastctfprimes(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        pastctfprimes_path = os.path.join(path, 'pastctfprimes.txt')
         primes = [long(x) for x in open('pastctfprimes.txt','r').readlines() if not x.startswith('#') and not x.startswith('\n')]
         if self.args.verbose:
             print "[*] Loaded " + str(len(primes)) + " primes"
