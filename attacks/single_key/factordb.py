0c4e9c6ad2fc923facc4faf4ea18d7c5c4c08f82
Ganapati <ganapati@foobar.baz>
2020-04-19 14:30:54 +0200

    handle factordb response

--- attacks/single_key/factordb.py
+++ attacks/single_key/factordb.py
@@ -12,6 +12,19 @@ from lib.exceptions import FactorizationError
 logger = logging.getLogger("global_logger")
 
 
+def egcd(a, b):
+    if a == 0:
+        return [b, 0, 1]
+    else:
+        g, y, x = egcd(b % a, a)
+        return [g, x - (b // a) * y, y]
+
+
+def modInv(a, m):
+    g, x, y = egcd(a, m)
+    return x % m
+
+
 def solveforp(equation):
     """Parse factordb response
     """
@@ -40,6 +53,7 @@ def attack(attack_rsa_obj, publickey, cipher=[]):
         r = s.get(url_1 % publickey.n, verify=False)
         regex = re.compile(r"index\.php\?id\=([0-9]+)", re.IGNORECASE)
         ids = regex.findall(r.text)
+
         # check if only 1 factor is returned
         if len(ids) == 2:
             # theres a chance that the only factor returned is prime, and so we can derive the priv key from it
@@ -52,29 +66,31 @@ def attack(attack_rsa_obj, publickey, cipher=[]):
                 priv_key = PrivateKey(e=int(publickey.e), n=int(publickey.n), d=d)
                 return (priv_key, None)
 
-        try:
-            regex = re.compile(r'value="([0-9\^\-]+)"', re.IGNORECASE)
-            p_id = ids[1]
-            r_1 = s.get(url_2 % p_id, verify=False)
-            key_p = regex.findall(r_1.text)[0]
-            publickey.p = int(key_p) if key_p.isdigit() else solveforp(key_p)
+        elif len(ids) == 3:
+            try:
+                regex = re.compile(r'value="([0-9\^\-]+)"', re.IGNORECASE)
+                p_id = ids[1]
+                r_1 = s.get(url_2 % p_id, verify=False)
+                key_p = regex.findall(r_1.text)[0]
+                publickey.p = int(key_p) if key_p.isdigit() else solveforp(key_p)
 
-            q_id = ids[2]
-            r_2 = s.get(url_2 % q_id, verify=False)
-            key_q = regex.findall(r_2.text)[0]
-            publickey.q = int(key_q) if key_q.isdigit() else solveforp(key_q)
-        except IndexError:
-            return (None, None)
+                q_id = ids[2]
+                r_2 = s.get(url_2 % q_id, verify=False)
+                key_q = regex.findall(r_2.text)[0]
+                publickey.q = int(key_q) if key_q.isdigit() else solveforp(key_q)
+            except IndexError:
+                return (None, None)
 
-        if publickey.p == publickey.q == publickey.n:
-            return (None, None)
-        priv_key = PrivateKey(
-            p=int(publickey.p),
-            q=int(publickey.q),
-            e=int(publickey.e),
-            n=int(publickey.n),
-        )
+            if publickey.p == publickey.q == publickey.n:
+                return (None, None)
+            priv_key = PrivateKey(
+                p=int(publickey.p),
+                q=int(publickey.q),
+                e=int(publickey.e),
+                n=int(publickey.n),
+            )
 
-        return (priv_key, None)
+            return (priv_key, None)
+        return (None, None)
     except:
         return (None, None)
