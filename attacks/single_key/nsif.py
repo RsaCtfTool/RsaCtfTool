51e4c798c5b01eddee5bda57be9d289502656df9
Pedro el banquero <60758685+pedroelbanquero@users.noreply.github.com>
2020-10-15 01:31:43 -0400

    Update nsif.py

--- attacks/single_key/nsif.py
+++ attacks/single_key/nsif.py
@@ -1,1 +1,26 @@
-#nsif python wraper
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+import subprocess
+from lib.timeout import timeout
+from lib.keys_wrapper import PrivateKey
+from lib.utils import rootpath
+
+
+logger = logging.getLogger("global_logger")
+
+
+def attack(attack_rsa_obj, publickey, cipher=[]):
+    try:
+        result = subprocess.run(["lib/bin/nsif", publickey.n, "/dev/null"], capture_output=True)
+
+    except (subprocess.CalledProcessError, subprocess.TimeoutExpired):
+        return (None, None)
+
+    if b"FAIL" not in result and b":" in result:
+        rresult = result.decode("utf-8").strip()
+        return ("carmichael derivate : "+rresult, None)
+    else:
+        return (None, None)
