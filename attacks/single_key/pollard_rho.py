c88852ef3f01945e67c6fbc063cce4b1885d4b00
Ganapati <ganapati@foobar.baz>
2020-11-06 20:11:21 +0100

    fracking indent

--- attacks/single_key/pollard_rho.py
+++ attacks/single_key/pollard_rho.py
@@ -7,21 +7,27 @@ from lib.keys_wrapper import PrivateKey
 
 from gmpy2 import *
 
-def pollard_rho(n, seed=2,p=2, mode=1):
-   if n % 2 == 0: return 2
-   if n % 3 == 0: return 3
-   if n % 5 == 0: return 5
-   if is_prime(n): return n
-   if mode ==1:
-     f=lambda x: x**p + 1
-   else:
-     f=lambda x: x**p - 1
-   x, y, d = seed, seed, 1
-   while d == 1:
-     x = f(x) % n
-     y = f(f(y)) % n
-     d = gcd((x - y) % n, n)
-   return None if d == n else d
+
+def pollard_rho(n, seed=2, p=2, mode=1):
+    if n % 2 == 0:
+        return 2
+    if n % 3 == 0:
+        return 3
+    if n % 5 == 0:
+        return 5
+    if is_prime(n):
+        return n
+    if mode == 1:
+        f = lambda x: x ** p + 1
+    else:
+        f = lambda x: x ** p - 1
+    x, y, d = seed, seed, 1
+    while d == 1:
+        x = f(x) % n
+        y = f(f(y)) % n
+        d = gcd((x - y) % n, n)
+    return None if d == n else d
+
 
 def attack(attack_rsa_obj, publickey, cipher=[]):
     """Run attack with Pollard Rho
