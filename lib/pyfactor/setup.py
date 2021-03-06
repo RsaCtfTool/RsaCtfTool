bdf5da6fd936bbf7174967c0c94e9ade6e92e058
vicent nos ripolles <vnos@blackhole.consulting>
2021-03-06 00:59:48 -0500

    delete gnfs

--- lib/pyfactor/setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# Use ./setup.py build_ext --inplace
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize(["fact_methods.pyx", "nt_utils.pyx"])
-)
