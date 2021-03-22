c5aa4bd40aeb62a1f928d390a6fbb98aa509612a
Dario Clavijo <clavijodario@gmail.com>
2021-03-22 12:03:18 -0300

    Improved execution time boundary: O(2n) instead of O(n^2)

--- attacks/multi_keys/commonfactors.py
+++ attacks/multi_keys/commonfactors.py
@@ -6,7 +6,17 @@ import itertools
 from lib.rsalibnum import gcd
 from lib.keys_wrapper import PrivateKey
 from lib.utils import timeout, TimeoutError
+from gmpy2 import mul
 
+def ProductTree(s):
+    l = len(s)
+    while l > 1:
+      if l % 2 != 0:
+        s+=[1]
+        l+=1
+      s = list(map(mul,s[0:l//2],s[l//2:]))
+      l =len(s)
+    return s[0]
 
 class Attack(AbstractAttack):
     def __init__(self, attack_rsa_obj, timeout=60):
@@ -20,36 +30,33 @@ class Attack(AbstractAttack):
 
         with timeout(self.timeout):
             try:
+                pubs = [pub.n for pub in publickeys]
                 # Try to find the gcd between each pair of moduli and resolve the private keys if gcd > 1
                 priv_keys = []
-                for x, y in itertools.combinations(publickeys, r=2):
-                    if x.n != y.n:
-                        g = gcd(x.n, y.n)
-                        if g != 1:
+                M = ProductTree(pubs)
+                for i in range(0,len(pubs)):
+                    pub = pubs[i]
+                    x = publickeys[i]
+                    R = M//pub
+                    g = gcd(pub,R)
+                    if pub > g > 1:
+                        try:
+                            p = g
+                            q = pub//g
+                            x.p = p
+                            x.q = q
+                            # update each attackobj with a private_key
+                            priv_key_1 = PrivateKey(
+                                int(x.p), int(x.q), int(x.e), int(x.n)
+                            )
+                            priv_keys.append(priv_key_1)
 
-                            try:
-                                # update each attackobj with a private_key
-                                x.p = g
-                                x.q = x.n // g
-                                y.p = g
-                                y.q = y.n // g
-                                priv_key_1 = PrivateKey(
-                                    int(x.p), int(x.q), int(x.e), int(x.n)
-                                )
-                                priv_key_2 = PrivateKey(
-                                    int(y.p), int(y.q), int(y.e), int(y.n)
-                                )
-                                priv_keys.append(priv_key_1)
-                                priv_keys.append(priv_key_2)
-
-                                self.logger.info(
-                                    "[*] Found common factor in modulus for "
-                                    + x.filename
-                                    + " and "
-                                    + y.filename
-                                )
-                            except ValueError:
-                                continue
+                            self.logger.info(
+                                "[*] Found common factor in modulus for "
+                                + x.filename
+                            )
+                        except ValueError:
+                            continue
             except TimeoutError:
                 return (None, None)
 
