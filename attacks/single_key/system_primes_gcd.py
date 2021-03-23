7dd917e04b3a2eccc1d5f2a4456e54048eb8c095
Dario Clavijo <clavijodario@gmail.com>
2021-03-23 01:25:19 -0300

    system_primes_gcd attack

---
+++ attacks/single_key/system_primes_gcd.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from attacks.abstract_attack import AbstractAttack
+from tqdm import tqdm
+from lib.keys_wrapper import PrivateKey
+from lib.utils import timeout, TimeoutError
+from lib.system_primes import load_system_consts
+from gmpy2 import gcd
+
+class Attack(AbstractAttack):
+    def __init__(self, attack_rsa_obj, timeout=60):
+        super().__init__(attack_rsa_obj, timeout)
+        self.speed = AbstractAttack.speed_enum["fast"]
+
+    def attack(self, publickey, cipher=[]):
+        """System primes in crypto constants"""
+        with timeout(self.timeout):
+            try:
+                primes = load_system_consts()
+                for prp in tqdm(primes):
+                    g = gcd(publickey.n, prp)
+                    if publickey.n > g > 1:
+                        publickey.q = g
+                        publickey.p = publickey.n // publickey.q
+                        priv_key = PrivateKey(
+                            int(publickey.p),
+                            int(publickey.q),
+                            int(publickey.e),
+                            int(publickey.n),
+                        )
+                        return (priv_key, None)
+            except TimeoutError:
+                return (None, None)
+        return (None, None)
