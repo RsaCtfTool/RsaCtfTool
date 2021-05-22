cad728828f18cf1f09920439ded276d2bfe90406
Dario Clavijo <clavijodario@gmail.com>
2021-05-21 21:25:26 -0300

    londahl.py: forgot -1

--- attacks/single_key/londahl.py
+++ attacks/single_key/londahl.py
@@ -26,7 +26,7 @@ class Attack(AbstractAttack):
 
         # check the table
         mu = invmod(powmod(2, phi_approx, n), n)
-        fac = (2 << b) % n
+        fac = (2 << (b-1)) % n
 
         for i in tqdm(range(0, b + 1), disable=(not progress)):
             if mu in look_up:
