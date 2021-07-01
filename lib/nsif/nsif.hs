a7d2ba9180e0556b78abdb926b9d92bcdfe45f61
peter bank <pedro@blackhole.consulting>
2021-07-01 10:49:51 -0500

    las libs , more fast methods, todo delete old shit

--- lib/nsif/nsif.hs
+++ lib/nsif/nsif.hs
@@ -20,6 +20,7 @@ import qualified Math.NumberTheory.Primes as P
 import Math.NumberTheory.ArithmeticFunctions
 import Math.NumberTheory.Powers.Modular
 import Math.NumberTheory.Powers.Squares
+import Math.NumberTheory.Powers
 import Codec.Crypto.RSA.Pure
 
 
@@ -33,8 +34,12 @@ primb n = read ((splitOn " " $ show (P.precPrime n)) !! 1)::Integer
 ncr n b e t=  head $ filter (\(r,w)-> w/=1 && w/=r ) $ map (\x-> (n, (gcd n ((powMod x (n^2-x^2) n) - x ))) )  $ map (\x-> (x+3))  [0..t]-- [(e^b..e^b+t] 
 
 
+loadprimes = do 
+	a<- readFile "../../../safes25.txt"
+	let b = splitOn "\n" a
+	let c = init $ map (\x-> read x::Integer) b
 
-
+	return c
 
 primc n l = map (\x-> read ((splitOn " " $ show (P.nextPrime x)) !! 1)::Integer) [1..l]
 
@@ -52,7 +57,18 @@ nsifc2 n base tries e
 -- search for best base
 -- search the best exponent
 
-intpowroot n = take 100 $ sort $ filter (\(r,f,g)-> (f^g)<n) $ concat $  map (\x-> map (\y-> (n-(y^x),y,x) ) [2..2000]) [2..112]
+powRoot n = minimum $ intpowroot (n)
+
+bestroot n = minimum $ nub $ sort $ filter (\(r,e,w)-> e/=n ) $  map (\x-> (n-((integerRoot x n))^x,(integerRoot x n),x) ) [1..1000]
+
+powDiv n o
+	| rest == 0 || rest == 1 || rest == 2 = (o++[(b,e)])
+	| otherwise = powDiv rest (o++[(b,e)])
+	where
+	(rest,b,e) = bestroot n
+		
+
+intpowroot n =  filter (\(r,f,g)-> (f^g)<=n) $ concat $  map (\x-> map (\y-> (n-(x^y),x,y) ) [2..512]) [2..512]
 
 -- loop "bit" squares
 --
@@ -74,9 +90,15 @@ nsifcrack n =
 
 bestpow n =  head $ filter (<n) $  map (\x-> map (\y-> (y^x) ) [1..1000]) [1..15]
 
+supcar primespair o
+	| lp == 1  = out
+	| otherwise = supcar to out
+	where
+	lp = length primespair
+	to = tail primespair
+	out = lcm (o) (head primespair)
 
-
-nsifc n base tries e
+nsifc n tries
 	| out2 /= 1 && out2 /= n = (div n out2,out2) 
 	| out /=1 && out /= n = (div n out,out)
 	| out3 /= 1 && out3 /= n = (div n out3,out3) 
@@ -88,18 +110,19 @@ nsifc n base tries e
 
 	| otherwise = (0,0)
 	where
-	
+ 	base = 2
+		
 	--(nearsquare) = 2^(logBase 2 n)
 	primesc = nub $ sort $ map prim [1..n]	
 	out =  head $ reverse ([1] ++ (filter (\r-> r/=1 && r/=2 ) $ map (\x-> gcd (n) (tryperiod ((n)) (x*(n + (n - x))) x)) $ reverse $ [2^base..2^base+tries]) )
-	out2 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^2-x^2) x)) [2^base..2^base+tries]))
+	out2 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^2-(x)^2) x)) [2^base..2^base+tries]))
 
-	out3 = head $ reverse ([1]++ (filter (\r-> r/=1 && r/=2 ) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^3+x^3) x)) [3^base..3^base+tries]))
-	out4 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^3-x^3) x)) [3^base..3^base+tries] ))
-	out5 =  head $ reverse ([1] ++ (filter (\r-> r/=1 && r/=2 ) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^5+x^5) x)) [5^base..5^base+tries]) )
-	out6 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^5-x^5) x)) [5^base..5^base+tries]))
-	out7 = head $ reverse ([1]++ (filter (\r-> r/=1 && r/=2 ) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^7+x^7) x)) [7^base..7^base+tries]))
-	out8 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^7-x^7) x)) [7^base..7^base+tries] ))
+	out3 = head $ reverse ([1]++ (filter (\r-> r/=1 && r/=2 ) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^3+(x)^3) x)) [3^base..3^base+tries]))
+	out4 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^3-(x)^3) x)) [3^base..3^base+tries] ))
+	out5 =  head $ reverse ([1] ++ (filter (\r-> r/=1 && r/=2 ) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^5+(x)^5) x)) [5^base..5^base+tries]) )
+	out6 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^5-(x)^5) x)) [5^base..5^base+tries]))
+	out7 = head $ reverse ([1]++ (filter (\r-> r/=1 && r/=2 ) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^7+(x)^7) x)) [7^base..7^base+tries]))
+	out8 = head $ reverse ([1]++ (filter (\x-> x/=1 && x/=2) $ map (\x-> gcd (n) (tryperiod ((n)) ((n)^7-(x)^7) x)) [7^base..7^base+tries] ))
 
 
 
@@ -142,14 +165,19 @@ primetosquare n = candidates ini (ini^2)
 intPowBaseExp n= head $  map (\[h,j,k]-> [k,h]) $ filter (\[e,r,u]-> r=="0") $ map (\([a,b],c)-> [a,b,show c]) $ tail $  map (\x-> (splitOn "." $ show $ (logBase x n),x)) [2..3000]
 
 
-nsif n tries = nsifc n bas tries exp 
+
+nsif n tries
+	| d /=1 && d /= n = (div n d,d,divcar)
+	| otherwise = (0,0,0)
 	where
+ 	base = 2 
+		
 	--(nearsquare) = 2^(logBase 2 n)
-	[b,e] = intPowBaseExp (fromInteger n)	
-	bas = read ("0"++b)::Integer
-
-	exp = read ("0"++e)::Integer
-
+	primesc = nub $ sort $ map prim [1..n]	
+	
+	out2 = head $ reverse ([(1,1)]++ (filter (\(r,u)-> r/=1 ) $ map (\x-> (gcd (n) ((tryperiod ((n)) ((n)^2-(x)^2) x)  ),x)) [2^base..2^base+tries]))
+	d = fst out2
+	divcar = snd out2
 
 
 --
@@ -164,7 +192,7 @@ main = do
     -- let e = args !! 2
     -- let m = args !! 3
 
-    let (factorA,factorB) = nsifc (read n::Integer) (read st::Integer) (read m::Integer) 2
+    let (factorA,factorB) = nsifc (read n::Integer) (read m::Integer) 
     
     putStrLn "Public Key" 
     
