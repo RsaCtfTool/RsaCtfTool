5664bef049d39abe33b3060beab3ef704e0a07ca
dclavijo <clavijodario@gmail.com>
2022-09-02 17:43:20 -0300

    move chinese_remainder function to lib/number_theory

--- attacks/multi_keys/hastads.py
+++ attacks/multi_keys/hastads.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from lib.number_theory import invert, chinese_remainder
 from attacks.abstract_attack import AbstractAttack
 from functools import reduce
 
@@ -10,42 +11,6 @@ class Attack(AbstractAttack):
         super().__init__(timeout)
         self.speed = AbstractAttack.speed_enum["medium"]
 
-    def chinese_remainder(self, n, a):
-        sum = 0
-        prod = reduce(lambda a, b: a * b, n)
-
-        for n_i, a_i in zip(n, a):
-            p = prod // n_i
-            sum += a_i * self.mul_inv(p, n_i) * p
-        return sum % prod
-
-    def mul_inv(self, a, b):
-        b0 = b
-        x0, x1 = 0, 1
-        if b == 1:
-            return 1
-        while a > 1:
-            q = a // b
-            a, b = b, a % b
-            x0, x1 = x1 - q * x0, x0
-        if x1 < 0:
-            x1 += b0
-        return x1
-
-    def find_invpow(self, x, n):
-        high = 1
-        while high**n < x:
-            high *= 2
-        low = high >> 1
-        while low < high:
-            mid = (low + high) >> 1
-            if low < mid and mid**n < x:
-                low = mid
-            elif high > mid and mid**n > x:
-                high = mid
-            else:
-                return mid
-        return mid + 1
 
     def attack(self, publickeys, cipher=[], progress=True):
         """Hastad attack for low public exponent
@@ -75,8 +40,8 @@ class Attack(AbstractAttack):
         if e != 3:
             return None, None
 
-        result = self.chinese_remainder(n, c)
-        nth = self.find_invpow(result, 3)
+        result = chinese_remainder(n, c)
+        nth = invert(result, 3)
 
         unciphered = []
         unciphered.append(nth.to_bytes((nth.bit_length() + 7) // 8, byteorder="big"))
