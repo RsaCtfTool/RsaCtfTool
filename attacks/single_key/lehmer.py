1428b674ae80eeea4f74dc6246762e71e37979ec
dclavijo <clavijodario@gmail.com>
2022-10-09 00:23:54 -0300

    add lehmer machine attack similar to fermat's attack

---
+++ attacks/single_key/lehmer.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from attacks.abstract_attack import AbstractAttack
+from lib.keys_wrapper import PrivateKey
+from lib.exceptions import FactorizationError
+from lib.number_theory import isqrt, is_square
+
+
+def lehmer_machine(n):
+    """
+    fermat based integer factorization
+    """
+    y = 1
+    while not is_square(n + y * y):
+        y += 1
+    x = isqrt(n + y * y)
+    return x - y, x + y
+
+
+class Attack(AbstractAttack):
+    def __init__(self, timeout=60):
+        super().__init__(timeout)
+        self.speed = AbstractAttack.speed_enum["medium"]
+
+        
+    def attack(self, publickey, cipher=[], progress=True):
+        """Run lehmer_machine attack with a timeout"""
+        try:
+            publickey.p, publickey.q = lehmer_machine(publickey.n)
+
+        except FactorizationError:
+            return None, None
+
+        if publickey.p is not None and publickey.q is not None:
+            try:
+                priv_key = PrivateKey(
+                    n=publickey.n,
+                    p=int(publickey.p),
+                    q=int(publickey.q),
+                    e=int(publickey.e),
+                )
+                return priv_key, None
+            except ValueError:
+                return None, None
+
+        return None, None
+
+    def test(self):
+        from lib.keys_wrapper import PublicKey
+
+        key_data = """-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCG6ZYBPnfEFpkADglB1IDARrL3
+Gk+Vs1CsGk1CY3KSPYpFYdlvv7AkBZWQcgGtMiXPbt7X3gLZHDhv+sKAty0Plcrn
+H0Lr4NPtrqznzqMZX6MsHGCA2Q74U9Bt1Fcskrn4MQu8DGNaXiaVJRF1EDCmWQgW
+VU52MDG8uzHj8RnGXwIDAQAB
+-----END PUBLIC KEY-----"""
+        result = self.attack(PublicKey(key_data), progress=False)
+        return result != (None, None)
