55855f499c1878943fc5a8a424d8c883e474e4c4
dclavijo <clavijodario@gmail.com>
2023-01-18 14:18:26 -0300

    simplify euler algorithm

--- attacks/single_key/euler.py
+++ attacks/single_key/euler.py
@@ -3,8 +3,45 @@
 
 from attacks.abstract_attack import AbstractAttack
 from lib.keys_wrapper import PrivateKey
-from lib.number_theory import gcd, isqrt, is_square, is_congruent
+from lib.number_theory import gcd, isqrt_rem, isqrt, is_congruent
 import logging
+
+
+def euler(n):
+    """
+    Euler factorization method is very much like fermat's
+    """
+    end = isqrt(n)
+    a = 0
+    b = 0
+    solutionsFound = []
+    firstb = -1
+
+    lf = 0
+    while a < end:
+        b, f = isqrt_rem(n - (a * a))
+        if f == 0 and (a != firstb) and (b != firstb):
+            solutionsFound.append([b, a])
+            firstb = b
+            lf = len(solutionsFound)
+            if lf == 2:
+                break
+        a += 1
+
+    if lf < 2:
+        return None
+
+    a = solutionsFound[0][0]
+    b = solutionsFound[0][1]
+    c = solutionsFound[1][0]
+    d = solutionsFound[1][1]
+
+    k = pow(gcd(a - c, d - b), 2)
+    h = pow(gcd(a + c, d + b), 2)
+    m = pow(gcd(a + c, d - b), 2)
+    l = pow(gcd(a - c, d + b), 2)
+
+    return gcd(k + h, n), gcd(l + m, n)
 
 
 class Attack(AbstractAttack):
@@ -12,46 +49,6 @@ class Attack(AbstractAttack):
         super().__init__(timeout)
         self.speed = AbstractAttack.speed_enum["slow"]
         self.logger = logging.getLogger("global_logger")
-
-    def euler(self, n):
-        """
-        Euler factorization method is very much like fermat's
-        """
-        if n & 1 == 0:
-            return (n >> 1, 2) if n > 2 else (2, 1)
-        end = isqrt(n)
-        a = 0
-        b = 0
-        solutionsFound = []
-        firstb = -1
-
-        lf = 0
-        while a < end:
-            bsquare = n - (a * a)
-            if is_square(bsquare):
-                b = isqrt(bsquare)
-                if (a != firstb) and (b != firstb):
-                    solutionsFound.append([b, a])
-                    firstb = b
-                    lf = len(solutionsFound)
-                    if lf == 2:
-                        break
-            a += 1
-
-        if lf < 2:
-            return None
-
-        a = solutionsFound[0][0]
-        b = solutionsFound[0][1]
-        c = solutionsFound[1][0]
-        d = solutionsFound[1][1]
-
-        k = pow(gcd(a - c, d - b), 2)
-        h = pow(gcd(a + c, d + b), 2)
-        m = pow(gcd(a + c, d - b), 2)
-        l = pow(gcd(a - c, d + b), 2)
-
-        return gcd(k + h, n), gcd(l + m, n)
 
     def attack(self, publickey, cipher=[], progress=True):
         """Run attack with Euler method"""
@@ -63,8 +60,7 @@ class Attack(AbstractAttack):
         # Euler attack
         try:
             if is_congruent(publickey.n, 1, 4):
-                euler_res = self.euler(publickey.n)
-                print(euler_res)
+                euler_res = euler(publickey.n)
             else:
                 self.logger.error(
                     "[!] Public key modulus must be congruent 1 mod 4 to work with euler method."
