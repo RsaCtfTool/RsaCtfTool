b2eeaeb06ad5059ceb601cd9918edfc28c5570c1
dclavijo <clavijodario@gmail.com>
2023-02-28 16:31:47 -0300

    lift lint python warnings

--- attacks/single_key/system_primes_gcd.py
+++ attacks/single_key/system_primes_gcd.py
@@ -32,8 +32,8 @@ class Attack(AbstractAttack):
                     )
                     return priv_key, None
                 else:
-                  self.logger.error("[!] Currently this tool only supports RSA textbook semiprime modulus, your p and q are: (%d,%d)" % (p,q))
-                  return None, None
+                    self.logger.error("[!] Currently this tool only supports RSA textbook semiprime modulus, your p and q are: (%d,%d)" % (p, q))
+                    return None, None
 
         return None, None
 
