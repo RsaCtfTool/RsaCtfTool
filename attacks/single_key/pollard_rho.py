d2a6eabfe892f2b4bf09aad29017691e8021f022
dclavijo <clavijodario@gmail.com>
2023-03-29 18:52:26 -0300

    simplify pollard_rho attack

--- attacks/single_key/pollard_rho.py
+++ attacks/single_key/pollard_rho.py
@@ -6,49 +6,33 @@ from attacks.abstract_attack import AbstractAttack
 from lib.keys_wrapper import PrivateKey
 from lib.number_theory import is_prime, gcd, powmod
 
+def pollard_rho(n):
+  d, x, y, g = 1, 2, 2, lambda x:powmod(x, 2, n)-1
+  while d == 1:
+    x, y = g(x), g(g(y))
+    d = gcd(abs(y-x), n)
+  return d
 
 class Attack(AbstractAttack):
     def __init__(self, timeout=60):
         super().__init__(timeout)
         self.speed = AbstractAttack.speed_enum["slow"]
 
-    def pollard_rho(self, n, seed=2, p=2, c=1):
-        f = lambda x: powmod(x, p, n) + c
-        x, y, d = seed, seed, 1
-        while d == 1:
-            x = f(x)
-            y = f(f(y))
-            d = gcd((x - y), n)
-            if n > d > 1:
-                return d
-
     def attack(self, publickey, cipher=[], progress=True):
         """Run attack with Pollard Rho"""
-        if not hasattr(publickey, "p"):
-            publickey.p = None
-        if not hasattr(publickey, "q"):
-            publickey.q = None
-
         # pollard Rho attack
         try:
-            try:
-                poll_res = self.pollard_rho(publickey.n)
-            except RecursionError:
-                print("RecursionError")
-                return None, None
+            p = pollard_rho(publickey.n)
+            publickey.p = p
+            publickey.q = publickey.n // publickey.p
 
-            if poll_res is not None:
-                publickey.p = poll_res
-                publickey.q = publickey.n // publickey.p
-
-            if publickey.q is not None:
-                priv_key = PrivateKey(
-                    int(publickey.p),
-                    int(publickey.q),
-                    int(publickey.e),
-                    int(publickey.n),
-                )
-                return priv_key, None
+            priv_key = PrivateKey(
+                int(publickey.p),
+                int(publickey.q),
+                int(publickey.e),
+                int(publickey.n),
+            )
+            return priv_key, None
         except TypeError:
             return None, None
 
