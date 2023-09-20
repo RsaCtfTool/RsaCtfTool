c03d9263542b1e90c2f1b05949b4c30cedb70e1b
dclavijo <clavijodario@gmail.com>
2023-09-20 13:44:18 -0300

    Add pickling, rapid7primes attack

---
+++ lib/pickling.py
@@ -0,0 +1,19 @@
+import bz2
+import pickle
+import _pickle as cPickle
+import sys
+
+
+# Pickle a file and then compress it into a file with extension 
+def compress_pickle(filename, data):
+ sys.stderr.write("saving pickle %s...\n" % filename)
+ with bz2.BZ2File(filename, 'w') as f: 
+   cPickle.dump(data, f)
+
+# Load any compressed pickle file
+def decompress_pickle(filename):
+ sys.stderr.write("loading pickle %s...\n" % filename)
+ data = bz2.BZ2File(filename, 'rb')
+ data = cPickle.load(data)
+ return data
+
