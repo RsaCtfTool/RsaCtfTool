245d5b7b73f1da60f82d53c4d4f2a65e3a17c69f
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2023-11-01 06:07:26 +0530

    Updated abstract_attack.py (#459)
    * Updated abstract_attack.py
    
    * Update

--- attacks/abstract_attack.py
+++ attacks/abstract_attack.py
@@ -4,25 +4,26 @@
 import logging
 from pathlib import Path
 import sys
-from lib.utils import timeout
+from typing import List, Any, Optional, Tuple
 import shutil
+from lib.utils import timeout
 
 
 class AbstractAttack(object):
-    speed_enum = {"slow": 0, "medium": 1, "fast": 2}
+    SPEED_ENUM = {"slow": 0, "medium": 1, "fast": 2}
 
-    def __init__(self, timeout=60):
+    def __init__(self, timeout: int = 60):
         self.logger = logging.getLogger("global_logger")
-        self.speed = AbstractAttack.speed_enum["medium"]
+        self.speed = AbstractAttack.SPEED_ENUM["medium"]
         self.timeout = timeout
         self.required_binaries = []
 
-    def get_name(self):
+    def get_name(self) -> str:
         """Return attack name"""
         full_path = sys.modules[self.__class__.__module__].__file__
         return Path(full_path).name.split(".")[0]
 
-    def can_run(self):
+    def can_run(self) -> bool:
         """Test if everything is ok for running attack"""
         for required_binary in self.required_binaries:
             if shutil.which(required_binary) is None:
@@ -33,11 +34,13 @@ class AbstractAttack(object):
                 return False
         return True
 
-    def attack(self, publickeys, cipher=[], progress=True):
+    def attack(self, publickeys: List[Any], cipher: Optional[List[Any]] = None, progress: bool = True) -> Tuple[Optional[Any], Optional[Any]]:
         """Attack implementation"""
+        if cipher is None:
+            cipher = []
         raise NotImplementedError
 
-    def attack_wrapper(self, publickeys, cipher=[], progress=True):
+    def attack_wrapper(self, publickeys: List[Any], cipher: Optional[List[Any]] = None, progress: bool = True) -> Tuple[Optional[Any], Optional[Any]]:
         """Attack wrapper to include timer in all attacks"""
         with timeout(self.timeout):
             try:
@@ -45,6 +48,9 @@ class AbstractAttack(object):
             except TimeoutError:
                 return None, None
 
-    def test(self):
+    def test(self) -> None:
         """Attack test case"""
         raise NotImplementedError
+
+# Configure logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
