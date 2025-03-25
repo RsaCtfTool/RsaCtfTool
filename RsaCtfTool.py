c51bda279192c49de010765f0cf8ff49e9a7f1e8
Darío Clavijo <clavijodario@gmail.com>
2025-03-25 17:18:25 -0300

    Update RsaCtfTool.py

--- RsaCtfTool.py
+++ RsaCtfTool.py
@@ -3,8 +3,8 @@
 
 """
 ----------------------------------------------------------------------------
-"THE BEER-WARE LICENSE" (Revision 42):
-ganapati (@G4N4P4T1) wrote this file. As long as you retain this notice you
+"MIT License:
+ganapati (@G4N4P4T1) is the original author of this project. As long as you retain this notice you
 can do whatever you want with this stuff. If we meet some day, and you think
 this stuff is worth it, you can buy me a beer in return.
 ----------------------------------------------------------------------------
