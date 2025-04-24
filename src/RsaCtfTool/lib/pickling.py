e772242fd771e8385180714853e41b98104dfb95
Darío Clavijo <clavijodario@gmail.com>
2025-04-24 17:43:00 -0300

    Attempt to mitigate RCE: https://zeroclick.sh/blog/rsactftool-rce

--- src/RsaCtfTool/lib/pickling.py
+++ src/RsaCtfTool/lib/pickling.py
@@ -2,7 +2,20 @@ import bz2
 import pickle
 import _pickle as cPickle
 import sys
+import io
 
+class SafeUnpickler(cPickle.Unpickler):
+    def find_class(self, module, name):
+        # Only allow safe built-in types
+        safe_builtins = {
+            'builtins': {'dict', 'list', 'str', 'int', 'float', 'set', 'tuple'}
+        }
+        if module in safe_builtins and name in safe_builtins[module]:
+            return super().find_class(module, name)
+        raise pickle.UnpicklingError(f"Unsafe module: {module}.{name}")
+
+def safe_load(file_obj):
+    return SafeUnpickler(file_obj).load()
 
 # Pickle a file and then compress it into a file with extension
 def compress_pickle(filename, data):
@@ -10,10 +23,8 @@ def compress_pickle(filename, data):
     with bz2.BZ2File(filename, "w") as f:
         cPickle.dump(data, f)
 
-
 # Load any compressed pickle file
 def decompress_pickle(filename):
     sys.stderr.write("loading pickle %s...\n" % filename)
-    data = bz2.BZ2File(filename, "rb")
-    data = cPickle.load(data)
-    return data
+    with bz2.BZ2File(filename, 'rb') as f:
+        return safe_load(f)  # from SafeUnpickler above
